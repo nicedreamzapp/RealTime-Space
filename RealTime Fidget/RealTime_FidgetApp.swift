1e33347b30da2d32734e7ab540d9b9d80ea1c61b
Matt Macosko <matt@inneedhemp.com>
2025-09-16 03:07:03 -0700

    Initial Commit

---
+++ RealTime Fidget/RealTime_FidgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  RealTime_FidgetApp.swift
+//  RealTime Fidget
+//
+//  Created by matthew macosko on 9/16/25.
+//
+
+import SwiftUI
+
+@main
+struct RealTime_FidgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
